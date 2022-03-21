fd2c6d12670d2d37cabededa7129bb6a5b316c8c
ramin-vakili <ramin.vaki@gmail.com>
2022-03-21 19:31:41 +0330

    Add staggered animations source file

---
+++ source/staggered-animations-in-flutter.dart
@@ -0,0 +1,65 @@
+// Want to support my work 🤝? https://buymeacoffee.com/vandad
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+  late final Animation<double> _rotateAnimation;
+  late final Animation<Offset> _shakeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..repeat(reverse: true);
+
+    _rotateAnimation = CurvedAnimation(
+      parent: _animationController,
+      curve: const Interval(0, 0.5, curve: Curves.easeInOut),
+    );
+
+    _shakeAnimation = Tween<Offset>(
+      begin: const Offset(0, 0),
+      end: const Offset(0, 0.2),
+    ).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: const Interval(0.5, 1, curve: Curves.elasticIn),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Staggered animations in Flutter'),
+      ),
+      body: Center(
+        child: RotationTransition(
+          turns: _rotateAnimation,
+          child: SlideTransition(
+              position: _shakeAnimation,
+              child: const Text('😊', style: TextStyle(fontSize: 56))),
+        ),
+      ),
+    );
+  }
+}
